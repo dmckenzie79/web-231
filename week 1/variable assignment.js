9b80aa74445c8f98957b8d47cfb756d4c8fb813f
Diandra McKenzie <diandramckenzie79@gmail.com>
2019-11-28 07:18:11 -0500

    Create variable assignment.js

---
+++ week 1/variable assignment.js
@@ -0,0 +1,18 @@
+/*============================================; 
+Title: Discussion 1.1 - Values and Variables; 
+Author: Diandra McKenzie; 
+Date: 28 November 2019; 
+Modified By: Classmates; 
+Description: This is a simple JavaScript program; 
+with the use of JavaScript values, and variables; 
+in an application. This program will not execute;
+and has at least two errors.;
+===========================================*/
+
+        //variables
+        var a = 15; 
+        var b = 10;
+        var x = baby;
+        
+        //output
+        document.write(A+b,x);
