d69b898d4965d08688ba26a68dd94ad76ef5c1eb
Diandra McKenzie <diandramckenzie79@gmail.com>
2019-11-28 09:19:40 -0500

    Create solution_verlee.js

---
+++ week 1/solution_verlee.js
@@ -0,0 +1,68 @@
+/*
+Title: Washington-assignment-1.3
+Author: Verlee Washington
+Date: 11/29/2019
+Modified by: Diandra McKenzie
+Description: A program that demonstrates the use of JavaScript types, values, 
+and variables in an application.
+*/
+
+// Start program.
+
+//Expected output:
+
+/*
+First name
+Last name 
+Age 
+Street 
+City 
+State 
+Zip code
+*/
+
+// The variable declaration and assignment.
+var firstName = "Verlee";
+var lastName = "Washington";
+var age = "\n37";
+var street  "\n1800 Learning St.";
+var city = "\nHome town";
+var state = "Bellevue";
+var zipCode = "89647";
+
+// The last line of code to write, the output.
+console.log (firstName, "" + lastName
++ age + street + city, "" + 
+state, "" + zipCode;
+
+// End program.
+
+
+//*Solution*
+
+ // Declared variables
+        
+        var firstName = "Verlee";
+        var lastName = "Washington"
+        var age = 37
+        var street = "1800 Learning St."
+        var city = "Hometown"
+        var state = "Bellevue" 
+        var zipcode = 89647
+
+// Output
+        
+        document.write(firstName + "<br>", lastName + "<br>", age + "<br>", street + "<br>", city + "<br>", state + "<br>", zipcode);
+  
+  
+  //*Detailed Explanation*
+  
+  The author created a string for each variable.
+  The first correction I made was to remove the quotes from the age and zipcode, these values are numbers, not text.
+  Next I removed \n from the age, street and city variables because I didnt think they were needed and to clean up the code.
+  I believe the author inserted \n to create a new line but I decided to tackle that in my output.
+  The author detailed to write the code so I changed the output to document.write instead of console.log, which would instead log the output in the console.
+  Lastly, I entered breaks in between each variable so the output would write each variable on a seperate line per the author's expected output.
+  
+  
+      
