5d7cfd82430f0018b0b92c88acbdf2e6bfa6485d
Diandra McKenzie <diandramckenzie79@gmail.com>
2019-11-28 16:45:25 -0500

    Adding mckenzie_primitive types.js

---
+++ mckenzie_primitive types.js
@@ -0,0 +1,37 @@
+
+/*
+Title: McKenzie_Assignment-1.4 Primitive Types
+Author: Diandra McKenzie
+Date: 2019 November 28
+Description: A program that demonstrates the use of JavaScript types, values, 
+and variables in an application.
+*/
+
+// Start program
+
+//Expected output:
+
+/*
+First name
+Last name 
+Age 
+Street 
+City  
+Zip code
+*/
+
+
+// Declared variables and assignments
+        
+        var firstName = "Diandra";
+        var lastName = "McKenzie"
+        var age = 40
+        var street = "12345 Pleasantville Blvd."
+        var city = "My Town" 
+        var zipcode = 11500
+
+// Output
+        
+        console.log(firstName, lastName, age, street, city, zipcode);
+  
+// End program
