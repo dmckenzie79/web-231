c306191fcd5bda8286ac3b4934cb5e6986cdc6d2
Diandra McKenzie <diandramckenzie79@gmail.com>
2019-11-28 14:36:56 -0500

    Update hello-world.js

--- week 1/hello-world.js
+++ week 1/hello-world.js
@@ -1,8 +1,8 @@
 /*
-; Title:  hello-world.js
-; Author: Diandra McKenzie
-; Date:   28 November 2019
-; Description: Displays message to the console window
+Title:  hello-world.js
+Author: Diandra McKenzie
+Date:   28 November 2019
+Description: Displays message to the console window
 */
 
 
