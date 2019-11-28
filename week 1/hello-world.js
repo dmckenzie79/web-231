edae1b8c607edf7d5228affcbff4804331769f6d
Diandra McKenzie <diandramckenzie79@gmail.com>
2019-11-28 14:13:31 -0500

    Adding assignment hello-world.js

---
+++ week 1/hello-world.js
@@ -0,0 +1,9 @@
+/*
+; Title:  hello-world.js
+; Author: Diandra McKenzie
+; Date:   28 November 2019
+; Description: Displays message to the console window
+*/
+
+
+console.log("Hello World, my name is Diandra McKenzie");
