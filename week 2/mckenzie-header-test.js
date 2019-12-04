b3b2c54cd8ae217442e4d518f061073c50749d24
Diandra McKenzie <diandramckenzie79@gmail.com>
2019-12-04 18:12:31 -0500

    adding header test file

---
+++ week 2/mckenzie-header-test.js
@@ -0,0 +1,24 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso 
+; Date:   4 December 2019
+; Modified By: Diandra McKenzie
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/*
+Expected Output
+Diandra McKenzie Week 2
+*/
+
+//Start Program
+
+const header = require('./mckenzie-header.js'); // Add import statement
+
+//Output
+
+console.log(header.display("Diandra", "McKenzie", "Week 2"));
+
+//End Program
