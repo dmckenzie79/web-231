fd014594120fddb6561cdb25d31b5077aa1d37c9
Diandra McKenzie <diandramckenzie79@gmail.com>
2019-12-07 13:46:26 -0500

    update

--- week 2/mckenzie-functions_2.4.js
+++ week 2/mckenzie-functions_2.4.js
@@ -2,7 +2,7 @@
 ============================================
 ; Title:  function-properties.js
 ; Author: Diandra McKenzie 
-; Date:   4 December 2019
+; Date:   7 December 2019
 ; Description: Demonstrates how to define and call function properties
 ;===========================================
 */
@@ -16,33 +16,82 @@
 
   Hello my name is Diandra McKenzie!
 
-  Today's date is 12/4/2019 and the current temperature is <33.6 - use the formatNumber function> degrees.
+  Today's date is 12/7/2019 and the current temperature is <33.6 - use the formatNumber function> degrees.
 
-  I am <40 - parsed int value> years old and my savings account goal is <500,000 - parsed float value> dollars.
+  I am <40 - parsed int value> years old and my savings account goal is <551000.75 - parsed float value> dollars.
 
 */ 
 
 //Start Program
 
-const header = require('./mckenzie-header.js'); // Import statement
-
-// function property definitions
-myName.diandra = "Diandra McKenzie"
+const header = require('./mckenzie-header.js'); // Import statement, Header
 
 /*
- * Params: none
+ * Params: firstName, lastName
  * Response: function property value
  * Description: returns the value assigned to a function property
  */
-function myName()   {
-    return myName.diandra
+
+function fullName(firstName, lastName) {
+  return firstName+" "+lastName;
 }
-    
+
+/*
+ * Params: year, month, day
+ * Response: function property value
+ * Description: returns the value assigned to a function property
+ */
+
+ function dateWriter(year,month,day){
+   return new Date();
+ }
+
+  /*
+ * Params: number, numOfFixedPositions
+ * Response: function property value
+ * Description: returns the value assigned to a function property
+ */
+
+function formatNumber(number,numOfFixedPositions) {
+    return number.toFixed(numOfFixedPositions);
+}
+
+/*
+ * Params: age
+ * Response: function property value
+ * Description: returns the value assigned to a function property
+ */
+
+function convertToInt(age) {
+  const parsed = parseInt(age);
+  return parsed;
+}
+
+/*
+ * Params: savings
+ * Response: function property value
+ * Description: returns the value assigned to a function property
+ */
+
+function convertToFloat(savings) {
+  const parsed = parseFloat(savings);
+  return parsed;
+}
+
+
 //Output
 console.log(header.display("Diandra", "McKenzie", "Exercise 2.4"));
 // new line
 console.log('\n')
 //Output
-console.log('Hello my name is', myName()+"!")
+console.log("Hello my name is"+" "+fullName("Diandra","McKenzie")+"!");
+// new line
+console.log('\n')
+//Output
+console.log("Today's date is"+" "+dateWriter(2019, 11, 7).toLocaleDateString()+" "+"and the current temperature is"+" "+formatNumber(33.57,1)+" "+"degrees.");
+// new line
+console.log('\n')
+//Output
+console.log("I am"+" "+convertToInt("40")+" "+"years old and my savings account goal is"+" "+convertToFloat("551000.75")+" "+"dollars.");
 
 //End Program
