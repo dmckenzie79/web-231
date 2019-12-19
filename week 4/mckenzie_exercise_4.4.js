8733f7e1d1ddf48c4f7789ab5511df33d48723cd
Diandra McKenzie <diandramckenzie79@gmail.com>
2019-12-18 19:48:06 -0500

    adding

---
+++ week 4/mckenzie_exercise_4.4.js
@@ -0,0 +1,86 @@
+/*
+============================================
+; Title: predicates.js
+; Author: Diandra McKenzie 
+; Date: 18 December 2019
+; Description: Demonstrates how to declare and and use JavaScript predicates
+;===========================================
+*/
+
+/*
+  Expected output:
+  FirstName LastName
+  Exercise 4.4
+  Today's Date
+
+  -- ORIGINAL ARRAY --
+  Alabama
+  Nebraska
+  Iowa
+  California
+  Nevada
+
+  -- SORTED ARRAY --
+  Alabama
+  California
+  Iowa
+  Nebraska
+  Nevada
+
+  -- SELECTED VALUE --
+  Iowa
+*/
+
+//Start Program
+
+// Import statement, Header
+const header = require('./mckenzie-header.js'); 
+ 
+//Output
+ console.log(header.display("Diandra", "McKenzie", "Exercise 4.4"));
+
+ // new line
+console.log("");
+
+
+// array of string values
+var states = ["Alabama", "Nebraska", "Iowa", "California", "Nevada"]
+
+/*
+ * Params: state, value
+ * Response: true/false
+ * Description: Returns true/false given the comparison
+ */
+
+//function
+ function getState(state, value) {
+    return state === value
+  }
+
+// output
+console.log("-- ORIGINAL ARRAY --");
+for (var x = 0; x < states.length; x++) {
+  console.log(states[x]);
+}
+
+// new line
+console.log("");
+
+// output
+console.log(" -- SORTED ARRAY --");
+states.sort().forEach(state => console.log(state));
+
+
+// new line
+console.log("");
+
+// output
+console.log(" -- SELECTED VALUE --");
+console.log(
+  states.filter // built-in JavaScript function for returning a subset of records
+  (
+    function (state) {
+      return getState(state, "Iowa") // use the getState function to return a matching state
+    }
+  )[0] // return the first element in the return array
+);
